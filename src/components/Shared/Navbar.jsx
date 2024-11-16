089434ce5a7be0b0cc218a78c48c14e83d238808
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2024-11-16 23:57:07 +0600

    fixed some bugs

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -26,14 +26,14 @@ const Navbar = () => {
                     <div className="navbar flex justify-evenly  max-w-screen-2xl mx-auto  z-50 rounded-xl">
                         <div className="navbar-start">
                             <div className="dropdown">
-                                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden p-0 mr-2 scale-125">
+                                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden px-3 mr-2 scale-125">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                                 </div>
-                                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 dark:bg-primary rounded-box w-36 flex items-center justify-center">
                                     {links}
                                 </ul>
                             </div>
-                            <Link href={`/`} className="btn btn-ghost text-2xl font-bold p-0">
+                            <Link href={`/`} className="btn btn-ghost text-2xl font-bold p-0 ml-2 lg:m-0">
                                 <cite className='text-primary dark:text-white'>Developer</cite>
                                 <cite className="text-secondary">Saif</cite>
                             </Link>
