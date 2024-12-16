0ee53a906b48d58497c1ab76ab2da1ed16ab6c40
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2024-12-17 01:35:25 +0600

    modefied footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,9 +1,6 @@
 'use client';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
-import { FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa';
-import Links from './Links';
 
 const Footer = () => {
   const pathName = usePathname();
@@ -13,32 +10,9 @@ const Footer = () => {
     <footer>
       {!isDashboard && (
         <div className="flex justify-center rounded bg-gray-200 text-base-content dark:bg-primary dark:text-white">
-          <div className="footer footer-center mx-auto max-w-screen-2xl p-10">
-            <nav className="text-xs">
-              <Links />
-            </nav>
-            <nav>
-              <div className="grid grid-flow-col gap-4">
-                <Link target="blank" href={`https://x.com/muhammadsaif77`}>
-                  <FaTwitter className="text-2xl text-[#0082C4] duration-500 hover:scale-125 hover:text-[#3cbbfa]" />
-                </Link>
-                <Link
-                  target="blank"
-                  href={`https://www.youtube.com/channel/UCH4scmKdKwWXeh5CUzdmddw`}
-                >
-                  <FaYoutube className="text-2xl text-[#0082C4] duration-500 hover:scale-125 hover:text-[#3cbbfa]" />
-                </Link>
-                <Link
-                  target="blank"
-                  href={`https://www.facebook.com/muhammadsaif7717`}
-                >
-                  <FaFacebook className="text-2xl text-[#0082C4] duration-500 hover:scale-125 hover:text-[#3cbbfa]" />
-                </Link>
-              </div>
-            </nav>
-            <aside>
-              <p>Copyright © 2024 - All right reserved by Developer Saif</p>
-            </aside>
+          <div className="footer gap-3 footer-center mx-auto max-w-screen-2xl p-10">
+              <p className='text-[20px]'>Developer Protfolio of MD. SAIF ISLAM</p>
+              <p className='text-[16px]'>Copyright © 2025 - All right reserved</p>
           </div>
         </div>
       )}
