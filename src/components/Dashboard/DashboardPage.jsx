2b269481aa51ce687cc0592d0ecce2f62138985e
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2024-12-26 00:44:04 +0600

    removed force-dynamic

--- src/components/Dashboard/DashboardPage.jsx
+++ src/components/Dashboard/DashboardPage.jsx
@@ -4,7 +4,7 @@ const DashboardPage = () => {
   return (
     <div className="min-h-[95vh] w-full bg-base-100 dark:bg-background">
       <div className="w-full bg-fuchsia-200 p-3 text-center">
-        <h1 className="text-5xl font-semibold">Welcome Cheaf!</h1>
+        <h1 className="text-5xl font-semibold">Welcome Chief!</h1>
         <p className="text-xl">Let{`'`}s customize your protfoliio</p>
       </div>
     </div>
