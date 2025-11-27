1cc5af6b0cf86c4f6e4c5c3017de4ed4289e3d07
MD. Saif Islam <muhammadsaif7717@gmail.com>
2025-11-28 02:32:29 +0600

    added banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from '@/components/root/About';
+import Banner from '@/components/root/Banner';
 import Projects from '@/components/root/Projects';
 import Skills from '@/components/root/Skills';
 
 export default function Home() {
   return (
     <main>
+      <Banner/>
       <About />
       <Skills />
       <Projects />
